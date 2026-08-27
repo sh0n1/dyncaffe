50c523fb83a528eaf35101dcfd978fba0d7c97b3
sh0n1 <n11077417@qut.edu.au>
2026-08-27 10:57:02 +1000

    new tifs

---
+++ run_caffe_serial_tests.py
@@ -0,0 +1,41 @@
+# ------------------------------------------------------------------------------
+# Dynamic CA-ffe
+# Copyright (C) 2022–2026 Maziar Gholami Korzani
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License,
+# or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+# See the GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see <https://www.gnu.org/licenses/>.
+# ------------------------------------------------------------------------------
+
+import numpy as np
+import sys
+sys.path.append("./src")
+from caffe import caffe  # NOQA
+
+
+if __name__ == "__main__":
+
+    input_DEM_file = './DEMs_1m/brisbane1.tif'
+    hf = 0.09
+    increment_constant = 1e-4
+    EV_threshold = 1e-5
+
+    # to make an instance of the caffe class model
+    sim = caffe(input_DEM_file)
+    sim.setConstants(hf, increment_constant, EV_threshold)
+    sim.ExcessVolumeArray(np.array([[499, 499, 2000]]))
+    sim.OpenBCArray(np.array([[300, 300]]))
+    sim.RunSimulation()
+    sim.setOutputPath("./DEMs_1m/")
+    sim.setOutputName("serial")
+    sim.CloseSimulation()
+
--- view_raster.py
+++ view_raster.py
@@ -36,5 +36,7 @@ if __name__ == "__main__":
     # view_raster(raster_path1)
     # view_raster(raster_path2)
     # view_raster(raster_path3)
-    raster_path = "./DEMs_1m/brisbane1.tif"
-    view_raster(raster_path)
+    raster_path1 = "./DEMs_1m/brisbane1.tif"
+    raster_path2 = "./DEMs_1m/serial_mwd.tif"
+    view_raster(raster_path1)
+    view_raster(raster_path2)
